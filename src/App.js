bed739e396f29f088636356c5e28e0e93f7384f0
cainpalmer <86445750+cainpalmer@users.noreply.github.com>
2021-09-08 19:12:02 -0400

    finished project day 1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 // Code Starts Here
 
-import React from "react";
+// Imports
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 import "./App.css";
 
+// App
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=Q7vYhSUm52jjNtAFWqK6wSu0dtkqAF6rdgBKHRyp")
+    .then(res => {
+      const data = res.data;
+      setData(data);
+    })
+    .catch(err => {
+      console.log("Error recieving Data");
+    })
+  },[])
+
+// Return
   return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+    <div className = "App">
+        <h1>Nasa's Photo of the Day: Andromeda</h1>
+      <div className = "AppInfo">
+        <img src = {data.hdurl} alt = "The Andromeda Galaxy"></img>
+        <h2>{data.title}</h2>
+        <h3>{data.date}</h3>
+      </div>
+        <p>{data.explanation}</p>
     </div>
   );
 }
